5644235ede86138733374c9ab601c6440a1c2b58
DonutDonkey <34782950+DonutDonkey@users.noreply.github.com>
2019-12-23 19:36:39 +0100

    +GUI script +Camera Pitch Angles transform smoothing +max armor hp values

--- Assets/Scripts/Actor/Player/Camera/FpsCameraPitchAngles.cs
+++ Assets/Scripts/Actor/Player/Camera/FpsCameraPitchAngles.cs
@@ -10,7 +10,7 @@ namespace Actor.Player.Camera {
         
         [SerializeField] private float maxAngle = 5f;
         [SerializeField] private float rate = 10f;
-        [SerializeField] private float maxDistance = 0.32f;
+        [SerializeField] private float movingOffsetReduction = 3.32f;
 
         private PlayerInputHandler _playerInputHandler;
 
@@ -22,29 +22,28 @@ namespace Actor.Player.Camera {
                                                   _playerInputHandler.GetVerticalMovement());
 
         private void CameraShift(float axisH, float axisV) {
-
             playerCamTransform.localRotation = SetLocalRotation(axisH);
             playerCamTransform.localPosition = SetLocalPosition(axisH,axisV);
         }
         
         private Quaternion SetLocalRotation(float axis) {
             var localRotation = playerCamTransform.localRotation;
-
+            
             localRotation = Quaternion.Lerp(
-                localRotation,
-                Quaternion.Euler(localRotation.x, localRotation.y, axis * maxAngle),
-                Time.deltaTime * rate);
-
+            localRotation,
+            Quaternion.Euler(localRotation.x, localRotation.y, axis * maxAngle),
+            Time.deltaTime * rate);
+            
             return localRotation;
         }
 
         private Vector3 SetLocalPosition(float axisX, float axisZ) {
             var localPosition = playerCamTransform.localPosition;
-
-            localPosition = new Vector3(
-                Mathf.Lerp(localPosition.x,axisX * maxDistance, rate),
-                localPosition.y,
-                Mathf.Lerp(localPosition.z, axisZ * maxDistance, rate));
+            
+            localPosition = Vector3.Lerp(
+                localPosition,
+                new Vector3(axisX / movingOffsetReduction, localPosition.y, axisZ / movingOffsetReduction),
+                Time.deltaTime * rate);
             
             return localPosition;
         }
---
+++ Assets/Scripts/GUI/GuiText.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using Data.Values;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GUI {
+    public class GuiText : MonoBehaviour {
+        [SerializeField] private FloatValue currentHp;
+        [SerializeField] private FloatValue currentArmor;
+        [SerializeField] private FloatValue maxHp;
+        [SerializeField] private FloatValue maxArmor;
+
+        [SerializeField] private Text currentHpText;
+        [SerializeField] private Text currentArmorText;
+
+        private void Awake() {
+            currentHpText.text = currentHp.value.ToString(CultureInfo.InvariantCulture);
+            currentArmorText.text = currentArmor.value.ToString(CultureInfo.InvariantCulture);
+        }
+
+        private void LateUpdate() {
+            if (currentHpText.text != currentHp.value.ToString(CultureInfo.InvariantCulture))
+                currentHpText.text = currentHp.value.ToString(CultureInfo.InvariantCulture);
+            
+            if (currentArmorText.text != currentArmor.value.ToString(CultureInfo.InvariantCulture))
+                currentArmorText.text = currentArmor.value.ToString(CultureInfo.InvariantCulture);
+        }
+    }
+}
