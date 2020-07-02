9e7fe6716b1e31e1d7e21ac7982d8d8fd6021654
KGM <34782950+DonutDonkey@users.noreply.github.com>
2020-07-02 16:18:34 +0200

    02.07.2020

--- Assets/Scripts/FiniteStateMachine.cs
+++ Assets/Scripts/FiniteStateMachine.cs
@@ -1,23 +1,28 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 public class FiniteStateMachine {
     private IState _currentState;
     
-    private List<Transition> _transitions; 
-    
+    private readonly List<Transition> _transitions;
+
+    public FiniteStateMachine(List<Transition> transitions) => _transitions = transitions;
+
     /**
      * Implements Default State
      */
     public void Start(IState state) => SetState(state);
 
     public void Tick() {
+        var transaction = getTransition();
+        
+        if( transaction != null )
+            SetState(transaction.To);
         
         _currentState?.Tick();
     }
 
-    public void SetState(IState state) {
+    private void SetState(IState state) {
         if(_currentState.Equals(state))
             return;
         
@@ -25,4 +30,8 @@ public class FiniteStateMachine {
         _currentState = state;
         _currentState?.OnEnter();
     }
+
+    private Transition getTransition() => 
+        _transitions.FirstOrDefault(transition => transition.Condition() 
+                                                  && transition.From.Equals(_currentState));
 }
