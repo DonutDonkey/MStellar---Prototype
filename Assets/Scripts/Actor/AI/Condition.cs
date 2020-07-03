5ca9bc5c706bdf06c84aa0b088e6871e8a78ed30
KGM <34782950+DonutDonkey@users.noreply.github.com>
2020-07-03 17:06:53 +0200

    03.07.2020

--- Assets/Scripts/Actor/AI/Condition.cs
+++ Assets/Scripts/Actor/AI/Condition.cs
@@ -2,6 +2,11 @@
 using UnityEngine;
 
 namespace Actor.AI {
+    /**
+     * Abstract condition class used to extend and create Scriptable Condition Object
+     * Each extensions needs to crete one single boolean function for that specific condition
+     * and then assign that method to the func field.
+     */
     public abstract class Condition : ScriptableObject {
         public Func<bool> IsTrue { get; }
     }
