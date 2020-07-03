6a1b39fd98f275b16681221dcc191dbf3b7a5fcd
KGM <34782950+DonutDonkey@users.noreply.github.com>
2020-07-03 17:28:56 +0200

    03.07.2020

--- Assets/Scripts/Actor/AI/Condition.cs
+++ Assets/Scripts/Actor/AI/Condition.cs
@@ -5,6 +5,8 @@ namespace Actor.AI {
      * Abstract condition class used to extend and create Scriptable Condition Object
      * Each new ScriptableObject that extends this class should implement the condition logic,
      * with will be used in transitions between states
+     *
+     * Each condition is basically a single IF check for that transition
      */
     public abstract class Condition : ScriptableObject {
         public abstract bool IsTrue();
