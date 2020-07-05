e8f22eedae502ddab9085b52d8d9602c55c3ec8f
KGM <34782950+DonutDonkey@users.noreply.github.com>
2020-07-05 19:17:19 +0200

    05.07.2020

---
+++ Assets/Scripts/Actor/AI/Conditions/Ai_C_CheckIfCanHear.cs
@@ -0,0 +1,27 @@
+﻿using Actor.Player;
+using Data.Values;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Actor.AI.Conditions {
+    public class Ai_C_CheckIfCanHear : Condition {
+        [SerializeField] private Transform actorTransform;
+        [SerializeField] private Transform targetTransform;
+
+        [SerializeField] private FloatValue value;
+
+        [SerializeField] private PlayerInputHandler playerInputHandler;
+
+        public override bool IsTrue() => (CheckDistance() && CheckIfMadeNoise()) 
+            ? true 
+            : false;
+
+        private bool CheckDistance() => 
+            Vector3.Distance(actorTransform.position, targetTransform.position) <= value;
+
+        private bool CheckIfMadeNoise() => 
+            (playerInputHandler.IsJumping() || playerInputHandler.GetAttackButton()) 
+                ? true 
+                : false;
+    }
+}
---
+++ Assets/Scripts/Actor/AI/Conditions/Ai_C_IsNotSeenOrHeard.cs
@@ -0,0 +1,33 @@
+﻿using Actor.Player;
+using Data.Values;
+using UnityEngine;
+
+namespace Actor.AI.Conditions {
+    public class Ai_C_IsNotSeenOrHeard : Condition {
+        [SerializeField] private FloatValue fieldOfView;
+        [SerializeField] private FloatValue viewRadius;
+
+        [SerializeField] private Transform thisTransform;
+        [SerializeField] private Transform otherTransform;
+
+        [SerializeField] private LayerMask viewMask;
+        
+        [SerializeField] private PlayerInputHandler playerInputHandler;
+        
+        public override bool IsTrue() => (!IsInFov() && !CheckIfMadeNoise()) 
+            ? true 
+            : false;
+
+        private bool IsInFov() => 
+            (Vector3.Distance(thisTransform.position, otherTransform.position) < viewRadius) 
+                ? true 
+                : false;
+
+        private float Angle() => Vector3.Angle(otherTransform.position - thisTransform.position, thisTransform.forward);
+        
+        private bool CheckIfMadeNoise() => 
+            (playerInputHandler.IsJumping() || playerInputHandler.GetAttackButton()) 
+                ? true 
+                : false;
+    }
+}
