99751373354aa58db859f2e7c436eed38d533a92
KGM <34782950+DonutDonkey@users.noreply.github.com>
2020-07-05 18:34:28 +0200

    05.07.2020

---
+++ Assets/Scripts/Actor/AI/Conditions/Ai_C_IsInFov.cs
@@ -0,0 +1,25 @@
+﻿using Data.Values;
+using UnityEngine;
+
+namespace Actor.AI.Conditions {
+    public class Ai_C_IsInFov : Condition {
+        [SerializeField] private FloatValue fieldOfView;
+        [SerializeField] private FloatValue viewRadius;
+
+        [SerializeField] private Transform thisTransform;
+        [SerializeField] private Transform otherTransform;
+
+        [SerializeField] private LayerMask viewMask;
+        
+        public override bool IsTrue() {
+            if (!(Vector3.Distance(thisTransform.position, otherTransform.position) < viewRadius)) 
+                return false;
+            if (!(Angle() < fieldOfView.value * 0.5f)) 
+                return false;
+            
+            return !Physics.Linecast(thisTransform.position, otherTransform.position, viewMask);
+        }
+        
+        private float Angle() => Vector3.Angle(otherTransform.position - thisTransform.position, thisTransform.forward);
+    }
+}
--- Assets/Scripts/Actor/Enemy/AI/EnemyDebug.cs
+++ Assets/Scripts/Actor/Enemy/AI/EnemyDebug.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using Data.Values;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace Actor.Enemy.AI {
     public class EnemyDebug : MonoBehaviour {
         [SerializeField] private FloatValue hearingRadius;
-        
+        [SerializeField] private FloatValue fieldOfView;
+
         [SerializeField] private Text text;
+
+        [SerializeField] private Transform debugTransform;
+
+        public LayerMask layerMask;
+        
         public Color HearingColor { get; set; } = Color.gray;
 
         public Text DebugText { get => text; set => text = value; }
@@ -18,6 +25,51 @@ namespace Actor.Enemy.AI {
             //Hearing zone
             Gizmos.DrawWireSphere(transform.parent.position, hearingRadius);
             
+            //Fov
+            DebugDrawFov();
         }
+        
+        public int steps;
+        
+        private void DebugDrawFov() {
+            var arcSteps = Mathf.RoundToInt(fieldOfView / steps);
+            
+            var fovColor = HearingColor;
+            fovColor.a = 0.24f;
+            Handles.color = fovColor;
+            
+            for (var i = 0; i <= arcSteps; i++) {
+                
+                var angle = fieldOfView.value - (i * 10);
+                Handles.DrawSolidArc(debugTransform.position,
+                    debugTransform.up,
+                    GetStartAngle(angle * 0.5f), // rotate from forward by step
+                    -arcSteps,
+                    GetRadiusDistance(angle * 0.5f));                
+                
+                Handles.DrawSolidArc(debugTransform.position,
+                    debugTransform.up,
+                    GetStartAngle(-angle * 0.5f), // rotate from forward by step
+                    arcSteps,
+                    GetRadiusDistance(-angle * 0.5f));
+            }
+        }
+
+        private Vector3 GetStartAngle(float startAngle) {
+            return Quaternion.AngleAxis(startAngle, Vector3.up) * debugTransform.forward;
+        }
+
+        private float GetRadiusDistance(float startAngle) {
+            Physics.Raycast(
+                debugTransform.position, //Where cast is starting
+                GetStartAngle(startAngle), //With direction it is going
+                out var hit, //hit point
+                layerMask); // obstacles
+            
+                return ( Vector3.Distance(debugTransform.position, hit.point) > hearingRadius) 
+                    ? hearingRadius 
+                    : Vector3.Distance(debugTransform.position, hit.point);
+        }
+        
     }
 }
