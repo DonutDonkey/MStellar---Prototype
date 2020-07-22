8967f0d7f25b802b9b55672987fa89243c2bfda5
KGM <34782950+DonutDonkey@users.noreply.github.com>
2020-07-22 09:27:55 +0200

    22.07.2020

---
+++ Assets/Scripts/GUI/Ui_Player_Completion.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace GUI {
+    public class Ui_Player_Completion : MonoBehaviour {
+        [SerializeField] private Text enemiesTotal;
+        [SerializeField] private Text enemiesCurrent;
+
+        private void Start() {
+            enemiesTotal.text = S_GameManager_LocalObserver.Instance.EnemyCountTotal.ToString();
+            enemiesCurrent.text = S_GameManager_LocalObserver.Instance.EnemyCountAlive.ToString();
+        }
+
+        private void LateUpdate() {
+            if (enemiesCurrent.text != S_GameManager_LocalObserver.Instance.EnemyCountAlive.ToString())
+                enemiesCurrent.text = S_GameManager_LocalObserver.Instance.EnemyCountAlive.ToString();
+        }
+    }
+}
