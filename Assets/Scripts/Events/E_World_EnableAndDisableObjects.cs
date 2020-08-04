95fc956bc65ad0130464c6f8a31394c202c49c70
KGM <34782950+DonutDonkey@users.noreply.github.com>
2020-08-04 14:42:38 +0200

    04.08.2020

---
+++ Assets/Scripts/Events/E_World_EnableAndDisableObjects.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Events {
+    public class E_World_EnableAndDisableObjects : MonoBehaviour {
+        [SerializeField] private List<GameObject> objectsToDisable;
+        [SerializeField] private List<GameObject> objectsToEnable;
+
+        public string otherColliderName;
+        
+        private void OnTriggerEnter(Collider other) {
+            if (!other.name.Equals(otherColliderName)) return;
+
+            foreach (var loopObj in objectsToDisable)
+                loopObj.SetActive(false);
+
+            foreach (var loopObj in objectsToEnable)
+                loopObj.SetActive(true);
+        }
+    }
+}
